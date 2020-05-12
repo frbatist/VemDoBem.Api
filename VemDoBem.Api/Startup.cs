16c0425fd55e702a0c2e2e4fcc59a63da2e2d782
Fernando Romano Batista <fbatist@hotmail.com>
2020-05-12 11:45:39 -0300

    pacotes nuget diagrama relacionamentos

--- VemDoBem.Api/Startup.cs
+++ VemDoBem.Api/Startup.cs
@@ -25,6 +25,9 @@ namespace VemDoBem.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlite(
+            //        Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
