79e0eb896376093687d09ab0092fa28a31af7b9f
Fernando Romano Batista <fbatist@hotmail.com>
2020-05-13 15:10:16 -0300

    cometi um equivoco, coloquei coisas de voluntário junto com a entidade de usuário

--- VemDoBem.Domain/Entidades/Usuario.cs
+++ VemDoBem.Domain/Entidades/Usuario.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
 using VemDoBem.Domain.ObjetosDeValor;
 
 namespace VemDoBem.Domain.Entidades
@@ -8,11 +7,6 @@ namespace VemDoBem.Domain.Entidades
     {
         public string Nome { get; set; }
         public Endereco Endereco { get; set; }
-        public byte[] Foto { get; set; }        
-        public IReadOnlyCollection<Experiencia> Experiencias => _experiencias.AsReadOnly();
-        public IReadOnlyCollection<TipoTrabalho> PreferenciasDeTrabalho => _preferenciasDeTrabalho.AsReadOnly();
-
-        private List<Experiencia> _experiencias = new List<Experiencia>();
-        private List<TipoTrabalho> _preferenciasDeTrabalho = new List<TipoTrabalho>();
+        public byte[] Foto { get; set; }
     }
 }
