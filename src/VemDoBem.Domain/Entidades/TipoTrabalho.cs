d3ebd0d064477de892c54241c0dfecff1ecd5e56
Fernando Romano Batista <fbatist@hotmail.com>
2020-05-22 17:05:23 -0300

    incluido construtor na entidade de tipo de trabalho e testes unitarios garantindo a construção válida

--- src/VemDoBem.Domain/Entidades/TipoTrabalho.cs
+++ src/VemDoBem.Domain/Entidades/TipoTrabalho.cs
@@ -1,7 +1,20 @@
-﻿namespace VemDoBem.Domain.Entidades
+﻿using System;
+
+namespace VemDoBem.Domain.Entidades
 {
     public class TipoTrabalho : Entidade<short>
     {
-        public string Descricao { get; set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+
+        const string MensagemErro = "Tipo de trabalho inválido. É necessário informar a descrição!";
+
+        public TipoTrabalho(string descricao)
+        {
+            if (string.IsNullOrWhiteSpace(descricao))
+                throw new InvalidOperationException(MensagemErro);
+            Descricao = descricao;
+            Ativo = true;
+        }
     }
 }
