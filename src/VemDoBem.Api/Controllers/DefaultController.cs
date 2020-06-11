56141cf10b00ec9e9c0f3cf4112704f00671f275
Fernando Romano Batista <fbatist@hotmail.com>
2020-06-11 16:59:11 -0300

    docker compose database, depois vejo para configurar o compose com o app junto e debugar pelo docker

---
+++ src/VemDoBem.Api/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VemDoBem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class DefaultController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("DEU BOA!");
+        }        
+    }
+}
